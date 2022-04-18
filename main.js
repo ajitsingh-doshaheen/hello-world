32c6f905267c7de5c53b40df74d4087d0adf1ceb
ajitsingh-doshaheen <97938471+ajitsingh-doshaheen@users.noreply.github.com>
2022-04-19 01:06:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 // Constants
 const PORT = 8000;
-const HOST = '0.0.0.0';
 
 // App
 const app = express();
@@ -10,6 +9,6 @@ app.get('/', (request, response) => {
   response.send('Hello World');
 });
 
-//app.listen(PORT, HOST);
-app.listen(PORT);
-console.log('Running on http://${HOST}:${PORT}');
+app.listen(PORT, () => {
+    console.log(`This app is listening at http://localhost:${PORT}`)
+})
